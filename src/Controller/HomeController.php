6e1406d5024d161e7fa1f7fe560284a7b3084443
YAHIAOUIanis <yahiaoui.anis11@gmail.com>
2019-02-11 11:23:28 +0100

    adding route annotations

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,10 +10,14 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController
 {
 
+    /**
+     * @Route("/")
+     */
     public function homePage(){
         return new Response("My first Symfony php !");
     }
