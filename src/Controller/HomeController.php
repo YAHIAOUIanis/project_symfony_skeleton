def030fc0adcfa1b7a81c7ae97386ffc44fd2311
YAHIAOUIanis <yahiaoui.anis11@gmail.com>
2019-02-11 10:57:59 +0100

    create HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anis
+ * Date: 11/02/2019
+ * Time: 10:32
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController
+{
+
+    public function HomeController(){
+        return new Response("My first Symfony php !");
+    }
+
+}
